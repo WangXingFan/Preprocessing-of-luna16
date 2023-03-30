29493f93a978a97b8b5523008b7487fc5f710a8e
WangXingFan <wangxingfan993@gmail.com>
2023-03-30 23:37:03 +0800

    update

--- files_rename.py
+++ files_rename.py
@@ -1,10 +1,30 @@
 from glob import glob
+import os
 
 import numpy as np
 
 
-path = "E:/VSCode/npy/npy0/"
+root_path = "E:/VSCode/npy/"
 
-trainimage = np.load(path + "trainImages.npy")
+images_list = []
+lungmasks_list = []
+masks_list = []
 
-print(len(trainimage))
+for i in range(10):
+    npy_path = root_path + "npy" + str(i) + "/"
+    img_files = glob(npy_path + "images_*.npy")
+    lung_files = glob(npy_path + "lungmask_*.npy")
+    mask_list = glob(npy_path + "masks_*.npy")
+
+    for f in img_files:
+        images_list.append(f)
+    for f in lung_files:
+        lungmasks_list.append(f)
+    for f in mask_list:
+        masks_list.append(f)
+
+print(len(images_list),len(lungmasks_list),len(masks_list))
+
+img = images_list[100]
+img = os.path.basename(img)
+print(img)
